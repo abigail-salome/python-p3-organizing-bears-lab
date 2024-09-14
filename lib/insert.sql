4ee5a4294cfaacefa19f9cbbd031579cf1b60bed
Abigail Salome <salomeabigail9@gmail.com>
2024-09-15 00:20:00 +0300

    writes code on python-p3-organizing-bears-lab

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+    ('Mr. Chocolate', 10, 'M', 'brown', 'calm', 1),
+    ('Rowdy', 5, 'M', 'black', 'aggressive', 1),
+    ('Tabitha', 7, 'F', 'white', 'curious', 1),
+    ('Sergeant Brown', 3, 'M', 'brown', 'brave', 1),
+    ('Melissa', 8, 'F', 'brown', 'friendly', 1),
+    ('Grinch', 12, 'M', 'green', 'grumpy', 0),
+    ('Wendy', 4, 'F', 'black', 'shy', 1),
+    (NULL, 1, 'M', 'brown', 'unknown', 1);
